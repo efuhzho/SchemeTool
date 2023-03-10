86dbe3a73a477eef9bc5f8143ac167666f8728f8
Marco <fuhong.zhou@qq.com>
2023-03-10 18:25:44 +0800

    .

---
+++ src/schemepresetwidget.cpp
@@ -0,0 +1,52 @@
+﻿#include "schemepresetwidget.h"
+
+#include <QLayout>
+#include <QFormLayout>
+//#include <>
+
+SchemePresetWidget::SchemePresetWidget(Preset* presetValue,QWidget *parent)
+    : QWidget{parent}
+{
+    m_presetValue = presetValue;
+    initUi();
+}
+
+void SchemePresetWidget::initData()
+{
+    lableName->setText(m_presetValue->name);
+}
+
+QString SchemePresetWidget::name()
+{
+    return m_presetValue->name;
+}
+
+void SchemePresetWidget::initUi()
+{
+    QVBoxLayout* vLayout = new QVBoxLayout(this);
+    vLayout->addWidget(lableName);
+
+    //add rated voltage and current value
+    {
+        QHBoxLayout* ratedValueLayout {new QHBoxLayout};
+
+        QFormLayout* voltLayout = new QFormLayout;
+        voltLayout->addRow(tr("Rated Voltage"),spinboxVolt);
+
+        QFormLayout* currLayout = new QFormLayout;
+        currLayout->addRow(tr("Rated Current"),spinboxCurr);
+
+        spinboxVolt->setSuffix("V");
+        spinboxCurr->setRange(1000,0);
+        spinboxCurr->setSuffix("A");
+
+        ratedValueLayout->addLayout(voltLayout);
+        ratedValueLayout->addSpacing(20);
+        ratedValueLayout->addLayout(currLayout);
+        ratedValueLayout->addStretch();
+        vLayout->addLayout(ratedValueLayout);
+    }
+
+
+    vLayout->addStretch();
+}
---
+++ src/schemepresetwidget.h
@@ -0,0 +1,33 @@
+﻿#ifndef SCHEMEPRESETWIDGET_H
+#define SCHEMEPRESETWIDGET_H
+
+#include <QWidget>
+#include <QLineEdit>
+#include <QDoubleSpinBox>
+#include <QLabel>
+
+#include "src/schemeDefine.h"
+
+
+class SchemePresetWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SchemePresetWidget(Preset* presetValue,QWidget *parent = nullptr);
+    void initData();
+    QString name();
+signals:
+
+private://elements
+    QLabel* lableName{new QLabel};
+    QDoubleSpinBox* spinboxVolt{new QDoubleSpinBox};
+    QDoubleSpinBox* spinboxCurr{new QDoubleSpinBox};
+
+private://members
+    Preset* m_presetValue{nullptr};
+
+private://inits
+    void initUi();
+};
+
+#endif // SCHEMEPRESETWIDGET_H
